1a16a8a3819077024b002c4fbffe66b8a1765387
WPF437 <oleksandr.kasianchuk@motorolasolutions.com>
2022-08-03 13:08:44 +0300

    message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,11 @@ package main.java;
 
 class Main {
     public static void main(String[] args) {
+        int i = 0;
+        while(i != 10){
+            System.out.println(i);
+            i++;
+        }
         System.out.println("hello world");
     }
 }

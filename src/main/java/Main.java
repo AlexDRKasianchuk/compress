f905854c1044f2750967babbab063d2ed04c31a3
WPF437 <oleksandr.kasianchuk@motorolasolutions.com>
2022-08-03 13:11:48 +0300

    message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ class Main {
     public static void main(String[] args) {
         int i = 0;
         while(i != 10){
-            System.out.println(i);
+            System.out.println("Loop: "+i);
             i++;
         }
         System.out.println("hello world");
